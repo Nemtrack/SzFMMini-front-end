2cad0c5d54a7cd9cc55116291be99b3f53d5d4bc
Nemtrack <balazsgyenes10@gmail.com>
2023-09-30 23:25:09 +0200

    Add form for login and signup

--- tree-planter/src/app/app.component.ts
+++ tree-planter/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tree-planter';
+  loginData = {
+    name: '',
+    password: '',
+  };
+  showLoginForm = false;
+  onSubmit() {
+    // Handle form submission logic here
+    console.log('Form submitted:', this.loginData);
+    this.loginData = { name: '', password: '' };
+    // You can add further logic, such as sending the form data to an API
+  }
 }
