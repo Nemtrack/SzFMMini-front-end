1e898b68ddba9a38b0833772555b31f84b52026d
Nemtrack <balazsgyenes10@gmail.com>
2023-10-03 08:53:22 +0200

    Add animation to fade form and buttons

--- tree-planter/src/app/app.component.ts
+++ tree-planter/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [animate('1000ms ease-out', style({ opacity: 0 }))]),
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1000ms ease-in', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'tree-planter';
@@ -11,7 +23,16 @@ export class AppComponent {
     name: '',
     password: '',
   };
+  showButtons = true;
   showLoginForm = false;
+
+  toggleLoginForm() {
+    this.showButtons = false;
+    setTimeout(() => {
+      this.showLoginForm = true;
+    }, 1000);
+  }
+
   onSubmit() {
     // Handle form submission logic here
     console.log('Form submitted:', this.loginData);
