2b3079d507dc9be6d950f96260c3867df78613fc
Nemtrack <balazsgyenes10@gmail.com>
2023-10-04 11:53:57 +0200

    Add tree component

--- tree-planter/src/app/app.module.ts
+++ tree-planter/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TreeComponent],
   imports: [
     BrowserModule,
     FormsModule,
