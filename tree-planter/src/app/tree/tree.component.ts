97c53f4758d3a3313c1bb5432ef7abe8471533dd
Nemtrack <balazsgyenes10@gmail.com>
2023-10-13 19:26:42 +0200

    Prepopulate Trees[] in tree.component and display welcome message

--- tree-planter/src/app/tree/tree.component.ts
+++ tree-planter/src/app/tree/tree.component.ts
@@ -11,5 +11,22 @@ export class TreeComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  trees: Tree[] = [];
+  username = 'Kiss Pista';
+  trees: Tree[] = [
+    new Tree(
+      'Lomb',
+      'Szép lombkorona',
+      'https://media.istockphoto.com/id/470604022/photo/apple-tree-without-flowers-or-fruit-isolated-on-white.jpg?s=612x612&w=0&k=20&c=hIQ0YlXwNsaRW4empl_lK2roR2tKX7Rq7pjFqPJR3QA='
+    ),
+    new Tree(
+      'Tree',
+      'Szép lomb',
+      'https://media.istockphoto.com/id/470604022/photo/apple-tree-without-flowers-or-fruit-isolated-on-white.jpg?s=612x612&w=0&k=20&c=hIQ0YlXwNsaRW4empl_lK2roR2tKX7Rq7pjFqPJR3QA='
+    ),
+    new Tree(
+      'Eert',
+      'Nice tree:)',
+      'https://media.istockphoto.com/id/470604022/photo/apple-tree-without-flowers-or-fruit-isolated-on-white.jpg?s=612x612&w=0&k=20&c=hIQ0YlXwNsaRW4empl_lK2roR2tKX7Rq7pjFqPJR3QA='
+    ),
+  ];
 }
